75b8ea741e72d137c6867ec5b2a23fcf2eb98589
Magnus Paulson Erga <magnus.p.erga@gmail.com>
2016-12-17 16:58:58 +0100

    Update settings.js

--- Viscovery/scripts/extension/settings.js
+++ Viscovery/scripts/extension/settings.js
@@ -2,3 +2,7 @@ $("#btnScreenshot").click( function() {
   var numPages = $("#selectionNumPages").val();
   chrome.runtime.sendMessage({ "message": "number_of_pages_to_view_set", "numPages": numPages});
 });
+
+$('#isMarkedSelected').click(function() {
+    chrome.runtime.sendMessage({ "message": "mark_checkBox_clicked", "isChecked": this.checked});
+});
