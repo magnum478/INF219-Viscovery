bce1a4cbec705b9b8a7447be145a9d0d1f71eb25
Magnus Paulson Erga <magnus.p.erga@gmail.com>
2016-12-17 17:00:05 +0100

    Update background.js

--- Viscovery/scripts/extension/background.js
+++ Viscovery/scripts/extension/background.js
@@ -8,11 +8,20 @@ chrome.runtime.onMessage.addListener(
         chrome.tabs.create({url:request.pageUrl}, ()=> {console.log("opened new tab!")});
         chrome.tabs.update(googleTabID, {selected: true});
         break;
+        
       case "number_of_pages_to_view_set":
       // Send a message to the active tab (googleTab)
         pageManager.setPagesToView(parseInt(request.numPages));
         pageManager.nextResultPage();
         break;
+        
+       case "mark_checkBox_clicked":
+        chrome.tabs.sendMessage(googleTabID, {
+        "message" : "mark_checkBox_clicked", 
+        "isChecked": request.isChecked
+        });
+        break
+        
       case "google_page_loaded":
         googleTabID = sender.tab.id;
         pageManager = new PageManager(new GooglePage(sender.tab.id, request.searchQuery), request.urls);
